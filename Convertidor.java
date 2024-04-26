3685440f47f2ae2d6aaf4c17e26be3ced85d003d
Gab Shinoda <144291883+gabriestival@users.noreply.github.com>
2024-04-25 22:44:13 -0600

    Create Convertidor.java
    Código del la adquisición y lectura del API
    
    #ALURALATAM #ORACLENEXTEDUCATION

---
+++ Convertidor.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import com.google.gson.JsonObject;
+import com.google.gson.Gson;
+
+public class Convertidor{
+    private static final String API_URL = "https://api.exchangerate-api.com/v4/latest/USD";
+
+    public double conversor(double cantidad, String divisaOrigen, String divisaDestino)
+  {
+        try {
+            URL url = new URL(API_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) 
+            {
+                response.append(line);
+            }
+          
+            reader.close();
+            Gson gson = new Gson();
+            JsonObject jsonObject = gson.fromJson(response.toString(), JsonObject.class);
+            JsonObject rates = jsonObject.getAsJsonObject("rates");
+            double sourceToTargetRate = rates.get(divisaDestino).getAsDouble();
+            double convertedAmount = cantidad * sourceToTargetRate;            
+            return convertedAmount; 
+        }
+        catch (IOException e) 
+          {           
+            e.printStackTrace();
+            return -1; 
+        } 
+        catch (Exception e) 
+          {
+            // Manejar otros errores
+            e.printStackTrace();
+            return -1; 
+        }
+    }
+}
